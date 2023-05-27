2fdedf425457f3bf9a7c556a4c7976755a8f6fa0
Dusuna <dusunax@gmail.com>
2023-05-28 00:02:11 +0900

    test: check text hello for app render

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,9 +1,8 @@
-import React from 'react';
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import { render, screen } from "@testing-library/react";
+import App from "./App";
 
-test('renders learn react link', () => {
+test("renders hello", () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  const HelloElement = screen.getByText(/hello/i);
+  expect(HelloElement).toBeInTheDocument();
 });
