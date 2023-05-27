1b4bc7dd6083bbd728f65f3bb09e4826936cbb60
Dusuna <dusunax@gmail.com>
2023-05-28 02:38:00 +0900

    delete: App.test.tsx

--- src/App.test.tsx
+++
@@ -1,8 +0,0 @@
-import { render, screen } from "@testing-library/react";
-import App from "./App";
-
-test("renders hello", () => {
-  render(<App />);
-  const HelloElement = screen.getByText(/hello/i);
-  expect(HelloElement).toBeInTheDocument();
-});
