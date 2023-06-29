86713dcc4b6c3076fc997bb7bf6417ca637b7fc3
Dusuna <dusunax@gmail.com>
2023-06-29 18:12:23 +0900

    delete: remove suspense component on route.tsx to fix startTransition error

--- frontend/src/router/Router.tsx
+++ frontend/src/router/Router.tsx
@@ -1,8 +1,7 @@
-import { Suspense, lazy } from "react";
+import { lazy } from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 
 import MainPage from "@/pages/Main";
-const Loading = lazy(() => import("@/pages/common/Loading"));
 
 const DicomPage = lazy(() => import("@/pages/view/Dicom"));
 const ThreePage = lazy(() => import("@/pages/view/Three"));
@@ -12,18 +11,16 @@ const Convertor = lazy(() => import("@/pages/convertor/Convertor"));
 export default function Router() {
   return (
     <BrowserRouter>
-      <Suspense fallback={<Loading />}>
-        <Routes>
-          <Route path="/" element={<MainPage />} />
+      <Routes>
+        <Route path="/" element={<MainPage />} />
 
-          <Route path="/view/dicom" element={<DicomPage />} />
-          <Route path="/view/3d/:type" element={<ThreePage />} />
-          <Route path="/view/vtk" element={<VTKPage />} />
+        <Route path="/view/dicom" element={<DicomPage />} />
+        <Route path="/view/3d/:type" element={<ThreePage />} />
+        <Route path="/view/vtk" element={<VTKPage />} />
 
-          <Route path="/convertor" element={<Convertor />} />
-          <Route path="/convertor/:convert-type" element={<Convertor />} />
-        </Routes>
-      </Suspense>
+        <Route path="/convertor" element={<Convertor />} />
+        <Route path="/convertor/:convert-type" element={<Convertor />} />
+      </Routes>
     </BrowserRouter>
   );
 }
